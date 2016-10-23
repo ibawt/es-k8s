d25f83ef364a5f3d7a8c272f50c3f9ada3513252
Ian Quick <ian.quick@gmail.com>
2016-10-23 07:22:44 -0400

    things

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM busybox
+FROM scratch
 MAINTAINER Ian Quick <ian.quick@gmail.com>
 ADD es-k8s /
 ENTRYPOINT ["/es-k8s"]
--- main.go
+++ main.go
@@ -13,102 +13,83 @@ import (
 
 var (
 	apiHost          = "http://127.0.0.1:8001"
-	resourceEndpoint = "apis/ibawt.ca/v1/namespaces/default/elastic-search"
+	resourceEndpoint = "/apis/ibawt.ca/v1/namespaces/default/elasticsearchs"
 )
 
-type Event struct {
-	Type   string `json:"type"`
-	Object string `json:"object"`
-}
-type Metadata map[string]string
-
-type Object struct {
-	ApiVersion string
-	Kind       string
-	Metadata   Metadata
+// ElasticSearchEvent thingy
+type ElasticSearchEvent struct {
+	Type   string        `json:"type"`
+	Object ElasticSearch `json:"object"`
 }
 
-type ResourceAdapter interface {
-	OnInit(Metadata) error
-	OnChange(Metadata) error
-	OnDestroy(Metadata) error
+// ElasticSearch thingy
+type ElasticSearch struct {
+	APIVersion string                 `json:"apiVersion"`
+	Kind       string                 `json:"kind"`
+	Metadata   map[string]interface{} `json:"metadata"`
+	Spec       ElasticSearchSpec      `json:"spec"`
+}
+
+// ElasticSearchSpec thingy
+type ElasticSearchSpec struct {
+	NumDataNodes   int `json:"dataNodes"`
+	NumClientNodes int `json:"clientNodes"`
+	NumMasterNodes int `json:"masterNodes"`
+}
+
+// ElasticSearchList thingy
+type ElasticSearchList struct {
+	APIVersion string                 `json:"apiVersion"`
+	Kind       string                 `json:"kind"`
+	Metadata   map[string]interface{} `json:"metadata"`
+	Items      []ElasticSearch        `json:"items"`
 }
 
 const defaultTimeout = 5 * time.Second
 
-func watchEvents(quitChan <-chan bool) (<-chan Event, <-chan error) {
-	eventChan := make(chan Event)
-	errorChan := make(chan error)
+func watchEvents() {
+	client := http.Client{Timeout: 90 * time.Second}
+	endPoint := apiHost + resourceEndpoint + "?watch=true"
 
-	go func() {
-		client := http.Client{Timeout: defaultTimeout}
-		endPoint := apiHost + "/" + resourceEndpoint + "?watch=true"
-		for {
-		top:
-			resp, err := client.Get(endPoint)
-			if err != nil {
-				errorChan <- err
-			} else if resp.StatusCode != http.StatusOK {
-				log.WithField("StatusCode", resp.StatusCode).Warn("Invalid response sleeping...")
-				time.Sleep(5 * time.Second)
-			} else {
-				decoder := json.NewDecoder(resp.Body)
-				// keep decoding events
-				for {
-					select {
-					case <-quitChan:
-						log.Info("watchEvents rx'd a quit, exiting...")
-						err = resp.Body.Close()
-						if err != nil {
-							log.WithError(err).Warn("Error closing response body")
-						}
-						close(eventChan)
-						close(errorChan)
-						return
-					default:
-						var event Event
-						err = decoder.Decode(&event)
-						log.Info("Decoded an event!")
-						if err != nil {
-							errorChan <- err
-							err = resp.Body.Close()
-							if err != nil {
-								log.WithError(err).Warn("Error closing response body")
-							}
-							goto top
-						} else {
-							eventChan <- event
-						}
+	for {
+	top:
+		log.WithField("EndPoint", endPoint).Debug("Polling...")
+		resp, err := client.Get(endPoint)
+		if err != nil {
+			log.WithError(err).Warn("Get")
+		} else if resp.StatusCode != http.StatusOK {
+			log.WithField("StatusCode", resp.StatusCode).Warn("Invalid response sleeping...")
+		}
+		if err != nil || resp.StatusCode != http.StatusOK {
+			time.Sleep(5 * time.Second)
+		} else {
+			decoder := json.NewDecoder(resp.Body)
+			// keep decoding events
+			for {
+				var event ElasticSearchEvent
+				err = decoder.Decode(&event)
+
+				if err != nil {
+					log.WithError(err).Warn("Error in decode")
+					err = resp.Body.Close()
+					if err != nil {
+						log.WithError(err).Warn("Error closing response body")
 					}
+					goto top
 				}
+				// handle event
 			}
 		}
-	}()
-
-	return eventChan, errorChan
+	}
 }
 
 func main() {
-	quitChan := make(chan bool)
+	log.Info("es-k8s starting...")
+	log.SetLevel(log.DebugLevel)
 	signalChan := make(chan os.Signal)
 
-	eventChan, errChan := watchEvents(quitChan)
-
+	go watchEvents()
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-	for {
-		select {
-		case err := <-errChan:
-			log.WithError(err).Warn("Error in main loop")
-		case evt, ok := <-eventChan:
-			log.Infof("recieved event!: %v\n", evt)
-			if !ok {
-				log.Warn("Event Channel is closed exiting...")
-				return
-			}
-		case <-signalChan:
-			log.Info("Received signal exiting...")
-			quitChan <- true
-			return
-		}
-	}
+	<-signalChan
+	os.Exit(0)
 }
